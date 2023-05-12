b1c99ce26d234921b1c532a0abaccfff7af13abd
Alexandre ZANNI <alexandre@acceis.fr>
2023-05-12 17:13:29 +0200

    1st commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<p align="center">
+  <img src="https://www.acceis.fr/voy_content/uploads/2021/07/logo.svg" alt="Logo ACCEIS" width="200px">
+</p>
+
+Website for [AVCS](https://github.com/Acceis/vulnerable-code-snippets) repository.
---
+++ render.rb
@@ -0,0 +1,10 @@
+require 'erb'
+require 'json'
+require 'net/http'
+
+avcs_rhtml = File.read('index.rhtml')
+avcs_data = JSON.load_file('avcs.json')
+template = ERB.new(avcs_rhtml)
+File.open('index.html', 'w') do |out|
+  out.write(template.result(binding))
+end
